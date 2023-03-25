d2edf2a75df18db852dbd7b9f6937133380dbce3
reharish <58879939+reharish@users.noreply.github.com>
2023-03-25 13:05:17 +0530

    Snap: Proc-Filesystem

---
+++ Proc-FileSystem/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, send_file, redirect, Response
+import os.path
+import websocket,json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+        if 'page' in request.args:
+            page = 'static/'+request.args.get('page')
+            if os.path.isfile(page):
+                resp=send_file(page)
+                resp.direct_passthrough = False
+                if os.path.getsize(page) == 0:
+                    resp.headers["Content-Length"]=str(len(resp.get_data()))
+                return resp
+            else:
+                return "File not found"
+        else:
+                return redirect('http://bagel.htb:8000/?page=index.html', code=302)
+
+@app.route('/orders')
+def order(): # don't forget to run the order app first with "dotnet <path to .dll>" command. Use your ssh key to access the machine.
+    try:
+        ws = websocket.WebSocket()    
+        ws.connect("ws://127.0.0.1:5000/") # connect to order app
+        order = {"ReadOrder":"orders.txt"}
+        data = str(json.dumps(order))
+        ws.send(data)
+        result = ws.recv()
+        return(json.loads(result)['ReadOrder'])
+    except:
+        return("Unable to connect")
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8000)
+
